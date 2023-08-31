5987b2d6ea8a4131ef705ae714ff06e5e944ecab
Padhusree <padmasree.k.2020@gmail.com>
2023-08-31 20:09:15 +0530

    variable commit

--- helloworld.sh
+++ helloworld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-person1=padhhu
-person2=Dheeru
+person1=Nicky
+person2=Sweety
 echo "$person1: Hi $person2 Good Morning!"
 echo "$person2: Hello $person1 GOOD MORNING, EM TIFFIN?"
 echo "$person1: emo $person2 leyala inkaa"
